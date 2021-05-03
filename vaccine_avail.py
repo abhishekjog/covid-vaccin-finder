2acfbddf6a6d40559604029939914a4c98421eae
Abhishek Jog <49052188+abhishekjog@users.noreply.github.com>
2021-05-03 14:26:26 +0530

    Update vaccine_avail.py

--- vaccine_avail.py
+++ vaccine_avail.py
@@ -28,16 +28,19 @@ look_for = {
 GET_STATES = "https://cdn-api.co-vin.in/api/v2/admin/location/states"
 GET_DISTRICTS = "https://cdn-api.co-vin.in/api/v2/admin/location/districts/"
 GET_SLOTS = f"https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=districtid&date={look_for['DATE']}"
+
 response = requests.get(GET_STATES)
 if response.ok:
     df = pd.DataFrame(json.loads(response.text)["states"])
     state_id = (df.query(f"state_name == '{look_for['STATE']}'")["state_id"].iloc[0])
     GET_DISTRICTS += str(state_id)
     response = requests.get(GET_DISTRICTS)
+
     if response.ok:
         df = pd.DataFrame(json.loads(response.text)["districts"])
         district_id = (df.query(f"district_name == '{look_for['DISTRICT']}'")["district_id"].iloc[0])
         response = requests.get(GET_SLOTS.replace("districtid", str(district_id)))
+
         if response.ok:
             df = pd.DataFrame(json.loads(response.text)["centers"])
             df = df.explode("sessions")
@@ -49,9 +52,12 @@ if response.ok:
                 print(df[(df['min_age_limit'] == look_for['AGE']) & (df['pincode'].isin(look_for['PIN']))].sort_values('date'))
             else:
                 print(df[df['min_age_limit'] == look_for['AGE']].sort_values('date'))
+
         else:
             print("Could not fetch slot level")
+
     else:
         print("Could not fetch district level")
+
 else:
     print("Could not fetch state level")
